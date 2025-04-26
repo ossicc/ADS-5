88851ecee615a8da2d36ee4e2eb7b85db15d7e78
ossicc <ossic8881@gmail.com>
2025-04-26 09:08:27 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,40 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+#include <iostream>
+#include <stdexcept>
+
+template <typename T, int kSize>
 class TStack {
-  // добавьте код стека
+ private:
+  T data[kSize];
+  int top;
+
+ public:
+  TStack() : top(-1) {}
+
+  void push(const T& value) {
+    if (top >= kSize - 1) {
+      throw std::overflow_error("Stack overflow");
+    }
+    data[++top] = value;
+  }
+
+  T pop() {
+    if (top < 0) {
+      throw std::underflow_error("Stack underflow");
+    }
+    return data[top--];
+  }
+
+  T peek() const {
+    if (top < 0) {
+      throw std::underflow_error("Stack is empty");
+    }
+    return data[top];
+  }
+
+  bool isEmpty() const { return top < 0; }
 };
 
 #endif  // INCLUDE_TSTACK_H_
